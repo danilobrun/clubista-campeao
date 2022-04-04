3e6377f157e733b25a12f7a424cf8005b0f0a655
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-04-03 21:35:39 -0300

    finaliza modal + css hover btn

---
+++ script.js
@@ -0,0 +1,23 @@
+let nome = 'danilo'
+let sobrenome = 'brun'
+console.log(nome+'',sobrenome);
+
+console.log(`log na template string ${nome} ${sobrenome} \n aqui é a segunda linha`);
+console.log(`template string 1 linha
+template string linha 2`);
+
+
+
+const arr = [
+    obj = {
+    nome: "w",
+    idade: "20"
+    },
+    "ola",
+    "teste2"
+]
+
+console.log(arr);
+console.log(typeof(arr));
+console.log(arr[2]);
+console.log(arr[0]);
