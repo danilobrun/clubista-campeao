3fbf7a979e1c6fb5c000caa70c3a3610bbe13309
Danilo Souza Brun <danilo.brun@al.infnet.edu.br>
2022-04-04 23:32:55 -0300

    Altera escopo das nossas variávies

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+const cartSidebar = document.querySelector('.cart-sidebar') /*Seleciona .cart-sidebar*/
 function openSidebar () {
-    var cartSidebar = document.querySelector('.cart-sidebar') /*Seleciona .cart-sidebar*/
     cartSidebar.classList.add('cart-sidebar-open') /*na lista de classes do cartSidebar add uma nova classe definida em nosso CSS*/
 }
 function closeSidebar () {
-    var cartSidebar = document.querySelector('.cart-sidebar') /*Seleciona meu cart-sidebar*/
     cartSidebar.classList.remove('cart-sidebar-open') /*removemos a classe que faz o cart abrir*/
 }
 const btnCart = document.querySelector('#btn-cart') /*Selecionando o elemento sacola*/
